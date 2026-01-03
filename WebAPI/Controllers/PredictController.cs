0e73752540e570775d94785654b0e58050fb20b1
KaushalKTripathi <kmr.kaushal@gmail.com>
2026-01-03 16:22:43 +0530

    Binary Classification

---
+++ WebAPI/Controllers/PredictController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.ML;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PredictController : ControllerBase
+    {
+        private readonly PredictionEnginePool<ModelInput, ModelOutput> predictionEngine;
+        public PredictController(PredictionEnginePool<ModelInput, ModelOutput> _predictionEngine) {
+
+            predictionEngine = _predictionEngine;
+        }
+
+        [HttpPost]
+        public IActionResult PredictVal([FromBody] ModelInput input)
+        {
+            ModelOutput result = predictionEngine.Predict(modelName: "SalaryModel", input);
+
+            return Ok(new { YearsOfExperience = input.YearsExperience, PrecitedSalary = result.Salary });
+
+        }
+     
+    }
+}
---
+++ WebAPI/InOut.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.ML.Data;
+
+namespace WebAPI
+{
+    public class ModelInput
+    {
+        public float YearsExperience { get; set; }
+    }
+
+    public class ModelOutput
+    {
+        [ColumnName("Score")]
+        public float Salary;
+    }
+}
